2eca3947f409ea23ef8370bf16e097bbbe2f5d20
Gagandeep <gagan.ddun@gmail.com>
2023-04-06 21:19:50 +1000

    modal styling

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useCallback, useEffect, useState } from "react";
-
+import { IoMdClose } from "react-icons/io";
 interface ModalProps {
   isOpen?: boolean;
   onClose: () => void;
@@ -72,9 +72,78 @@ export const Modal = ({
         z-50
         outline-none
         focus:outline-none
-        bg-neutral-800/70
-    "
-      ></div>
+        bg-neutral-800/70"
+      >
+        <div
+          className="
+        relative
+        w-full
+        md:w-4/6
+        lg:w-3/6
+        xl:w-2/5
+        my-6
+        mx-auto
+        h-full
+        lg:h-auto
+        md:h-auto"
+        >
+          <div
+            className={`
+            translate
+            duration-300
+            h-full
+            ${showModal} ? 'translate-y-0' : 'translate-y-full'
+            ${showModal} ? 'opacity-100' : 'opacity-0'
+            `}
+          >
+            <div
+              className="
+            translate
+            h-full
+            lg:h-auto
+            md:h-auto
+            border-0
+            rounded-lg
+            shadow-lg
+            relative
+            flex
+            flex-col
+            w-full
+            bg-white
+            outline-none
+            focus:outline-none
+            "
+            >
+              {/* header */}
+              <div
+                className="
+                flex
+                items-center
+                p-6
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
+              "
+              >
+                <button
+                  className="
+                p-1
+                border-0
+                hover:opacity-70
+                transition
+                absolute
+                left-9"
+                  onClick={handleClose}
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <div className="text-lg font-semibold">Login Modal</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Navbar } from "components/navbar/Navbar";
-import { Modal } from "components/modals/modal";
+import { Navbar } from "../components/navbar/Navbar";
+import { Modal } from "../components/modals/Modal";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
