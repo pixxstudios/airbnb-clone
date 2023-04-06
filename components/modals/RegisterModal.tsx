058b499c9e9268b03bd2cad18f0d0d45b463f15f
Gagandeep <gagan.ddun@gmail.com>
2023-04-06 21:48:28 +1000

    Register modal

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import { useRegisterModal } from "../../hooks/useRegisterModal";
+
+export const RegisterModal = () => {
+  return <div></div>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Navbar } from "../components/navbar/Navbar";
-import { Modal } from "../components/modals/Modal";
+import { RegisterModal } from "../components/modals/RegisterModal";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal isOpen title="Hello" actionLabel="Submit" />
+      <RegisterModal />
       <Navbar />
       <Component {...pageProps} />
     </>
