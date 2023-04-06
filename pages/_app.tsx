320f9c40300ee7c3facff4ff099b0ce3d4151412
Gagandeep <gagan.ddun@gmail.com>
2023-04-06 21:02:54 +1000

    modal styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Navbar } from '../components/navbar/Navbar'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Navbar } from "components/navbar/Navbar";
+import { Modal } from "components/modals/modal";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Modal isOpen />
       <Navbar />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
