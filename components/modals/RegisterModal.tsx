49a7004741e85746f4d6fd2b46ab4951f303ee7f
Gagandeep <gagan.ddun@gmail.com>
2023-04-11 20:51:01 +1000

    form initial values

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
@@ -11,5 +11,20 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useRegisterModal } from "../../hooks/useRegisterModal";
 
 export const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
   return <div></div>;
 };
