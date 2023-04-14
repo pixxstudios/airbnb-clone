1a7e18cb1d24a45989f674db1e22c6cabbb42817
Gagandeep <gagan.ddun@gmail.com>
2023-04-14 21:12:31 +1000

    login link

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -83,6 +83,23 @@ export const RegisterModal = () => {
         icon={FcGoogle}
         onClick={() => {}}
       />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already have an account?</div>
+          <div
+            className="text-neutral-800 cursor-pointer hover:underline"
+            onClick={registerModal.onClose}
+          >
+            Login
+          </div>
+        </div>
+      </div>
     </div>
   );
 
